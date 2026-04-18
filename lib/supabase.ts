987da3a2c2eda07c268476a47c38a17a17ffc8db
seefar-app <seefar-app@users.noreply.github.com>
2026-04-18 22:18:44 +0100

    Update - 2026-04-18 21:18

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qpjxcuymamhqxpbgpryd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwanhjdXltYW1ocXhwYmdwcnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NTM4ODAsImV4cCI6MjA5MjAyOTg4MH0.yvgdC97uU9_OznjRHKnMre8krTsB4LPJV9zXdE79qGo';
+const supabaseUrl = 'https://bmpstecvpfxwickjaudj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtcHN0ZWN2cGZ4d2lja2phdWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MzM3NDAsImV4cCI6MjA5MjEwOTc0MH0.WmGUJCtIElbbu8JmwrqW1FA8DesAkKLVdHtEgc2mgUw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
